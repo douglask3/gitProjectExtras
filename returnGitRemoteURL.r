c8f14fa966cc64ef33c0a621f0cbf2980b1dfd4a
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-10-15 09:17:19 +1100

    missed one (see previous)

--- returnGitRemoteURL.r
+++ returnGitRemoteURL.r
@@ -1,6 +1,6 @@
 returnGitRemoteURL <- function(gitLoc=".git") {
 	gitLoc=findProjectPath(gitLoc)
-	fileName=file.path(git_loc,'config')
+	fileName=file.path(gitLoc,'config')
 	URL=readChar(fileName, file.info(fileName)$size)
 	URL=strsplit(URL,'url = ')[[1]][2]
 	
