5beadd43ad8825398a3ce17794e7f45a9912abf4
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-10-15 09:34:25 +1100

    adding more ddefualts dirs

--- setupProjectStructure.r
+++ setupProjectStructure.r
@@ -1,3 +1,3 @@
-setupProjectStructure <- function(namess=c('out_dir','data_dir','temp_dir'),
-									dirn=c('outputs','data','temp'))
+setupProjectStructure <- function(namess=c('out_dir','data_dir','temp_dir','figs_dir','docs_dir'),
+									dirn=c('outputs','data','temp','figs','docs'))
 	mapply(makeGlobDir,namess,dirn)
