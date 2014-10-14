401aaa3ecc530484b938b4d5fa08949f4bd764c8
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-10-15 09:12:24 +1100

    removing _ to help mvim

--- libs/findProjectPath.r
+++ libs/findProjectPath.r
@@ -1,10 +1,10 @@
-find_project_path <- function(path,ncall_max=10) {
+findProjectPath <- function(path,ncallMax=10) {
 	nc=0
-	while (!file.exists(path) && nc<(ncall_max+1)) {
+	while (!file.exists(path) && nc<(ncallMax+1)) {
 		path=file.path("..",path)
 		nc=nc+1
 	}
-	if (nc>ncall_max) stop("libs dir or file not found")
+	if (nc>ncallMax) stop("libs dir or file not found")
 	
 	return(path)
 }
