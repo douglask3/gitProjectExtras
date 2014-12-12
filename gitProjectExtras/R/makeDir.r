60b20891b398566d7cde9df8ca0ee8f207fb0ce0
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-12 14:16:56 +1100

    passing argemnents through to dir.create and updating manuals for makeDir

--- gitProjectExtras/R/makeDir.r
+++ gitProjectExtras/R/makeDir.r
@@ -1,6 +1,6 @@
-makeDir <- function(fname) if(!file.exists(fname)) dir.create(fname)
+makeDir <- function(fname, ...) if(!file.exists(fname)) dir.create(fname, ...)
 
-makeGlobDir <- function(obj, fname) {
-	makeDir(fname)
+makeGlobDir <- function(obj, fname, ...) {
+	makeDir(fname, ...)
 	assignGlobal(obj, fname)
 }
