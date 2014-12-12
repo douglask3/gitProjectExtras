29070ffe8185e61167133c835153d278da893116
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-12 11:12:49 +1100

    adding n as an arguemht to pass into sys.call to make swithcing function location easier

--- gitProjectExtras/R/match.call.string.r
+++ gitProjectExtras/R/match.call.string.r
@@ -1,2 +1,6 @@
-match.call.string <- function(call=sys.call(sys.parent(n = 1)),...)
-	as.character(match.call(call=call,...))[1]
+match.call.string <- function(n=1,call=sys.call(sys.parent(n = n)),...) {
+	FunName = as.character(match.call(call=call,...))[1]
+	if (FunName == "match.call.string")  FunName = "N/A"
+	return(FunName)
+}
+	
