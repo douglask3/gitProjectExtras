b7cd6a2cc5d0b3e7c3c38cf78152a38b0213ad8b
douglask3 - Reading <douglas.i.kelley@gmail.com>
2015-10-09 14:27:17 +0100

    adding git.log and git.checkout to git commands

--- gitProjectExtras/R/gitCommands.r
+++ gitProjectExtras/R/gitCommands.r
@@ -1,31 +1,35 @@
-git <- function(commands) {
+git          <- function(commands) {
     commands = paste(c('git', commands), collapse = ' ')
 
     system(commands)
 }
 
-git.push   <- function(...) git(c('push', ...))
-git.pull   <- function(...) git(c('pull', ...))
+git.push     <- function(...) git(c('push', ...))
+git.pull     <- function(...) git(c('pull', ...))
 
 
-git.status <- function(...) git(c('status', ...))
+git.status   <- function(...) git(c('status', ...))
 
 
-git.commit <- function(message, messageType = '-m', ...) {
+git.commit   <- function(message, messageType = '-m', ...) {
     message  = paste('"', message, '"', sep = "")
     commands = c('commit', messageType, ..., message)
 
     git(commands)
 }
 
-git.add    <- function(files ='.', ...) {
+git.add      <- function(files ='.', ...) {
     commands  = c('add', ..., files)
     git(commands)
 }
 
-git.addCommit <- function(files = '.', message, messageType = '-m',
+git.addCommit<- function(files = '.', message, messageType = '-m',
                           addOptions = '', commitOptions = '') {
 
     git.add   (files, addOptions)
     git.commit(message, messageType, commitOptions)
 }
+
+git.log      <- function(...) git(c('log', ...))
+
+git.checkout <- function(...) git(c('checkout', ...))
