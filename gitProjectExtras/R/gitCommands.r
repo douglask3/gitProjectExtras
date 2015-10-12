0e68178fd51281d11d94b8c3390b3e87794d3a9f
douglask3 - Reading <douglas.i.kelley@gmail.com>
2015-10-12 10:04:08 +0100

    git log returns log to variable of own defined class of print and summary

--- gitProjectExtras/R/gitCommands.r
+++ gitProjectExtras/R/gitCommands.r
@@ -2,6 +2,8 @@ git          <- function(commands) {
     commands = paste(c('git', commands), collapse = ' ')
 
     system(commands)
+    out = system(commands, intern = TRUE)
+    invisible(out)
 }
 
 git.push     <- function(...) git(c('push', ...))
@@ -30,6 +32,39 @@ git.addCommit<- function(files = '.', message, messageType = '-m',
     git.commit(message, messageType, commitOptions)
 }
 
-git.log      <- function(...) git(c('log', ...))
+git.log      <- function(...) {
+    out   = git(c('log', ...))
+    index = which(sapply(out, substr, 1,6) == "commit")
+    index = c(index, length(out) + 1)
+    commits = list()
+    for (i in 2:length(index)) {
+        commit = out[index[i - 1]:(index[i] - 1)]
+        class(commit) = 'gitRevision'
+        commits = c(commits, list(commit))
+    }
+    rnms = head(names(index), -1)
+    rnms = sapply(rnms, function(i) strsplit(i, 'commit ')[[1]][2])
+
+    names(commits) = rnms
+    class(commits) = 'gitRevisions'
+    invisible(commits)
+}
+
+print.gitRevision    <- function(x, ...) printCatN(x,...)
+
+print.gitRevisions   <- function(x, ...) {
+    printRevision <- function(i) {
+        print.gitRevision(i)
+        cat("=============================\n\n")
+    }
+    sapply(head(x, -1), printRevision)
+    print.gitRevision(tail(x,1))
+}
+
+summary.gitRevisions <- function(x, ...) printCatN(names(x), ...)
+
+
+printCatN <- function(x, ...)
+    invisible(sapply(x, function(i) cat(paste(i,'\n'), ...)))
 
 git.checkout <- function(...) git(c('checkout', ...))
