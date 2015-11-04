db10a3cc20c35c994e85ae5d50ae7ad5b75308f8
douglask3 - Reading <douglas.i.kelley@gmail.com>
2015-11-04 15:19:48 +0000

    fuction for adding git information to nc outputted by writeRaster in package raster

---
+++ gitProjectExtras/R/writeRaster.gitInfo.r
@@ -0,0 +1,39 @@
+writeRaster.gitInfo <- function(x, filename,
+	                            VersionNumber = TRUE, URL = TRUE,
+								comment = NULL, ...) {
+
+	x = writeRaster(x, filename, format= "CDF",...)
+
+	filename = filename(x)
+
+	if (filename != "")
+		addGitInfo2RasterFile(filename, VersionNumber, URL, timeAndDate, comment)
+	else {
+		filename = c()
+		for (i in 1:nlayers(x)) filename = c(filename, filename(x[[i]]))
+		filename = unique(filename)
+
+		sapply(filename, addGitInfo2RasterFile,
+			   VersionNumber, URL, timeAndDate, comment)
+	}
+	return(x)
+}
+
+addGitInfo2RasterFile <- function(filename, VersionNumber, URL,
+								  timeAndDate, comment) {
+
+	attPutStandard <- function(test, name, val)
+		if (test) ncatt_put(nc, 0, name, val)
+
+	nc = nc_open(filename, write = TRUE)
+		attPutStandard(VersionNumber, "git Revision Number", gitVersionNumber())
+		attPutStandard(URL          , "git Repository URL" , gitRemoteURL())
+		attPutStandard(TRUE         , "git info by"        , paste("gitProjectExtras",  packageVersion('gitProjectExtras')))
+
+		if (!is.null(comment)) {
+			if (is.null(names(comment)))
+				names(comment) = paste(comment, 1:length(comment))
+			mapply(attPutStandard, TRUE, names(comment), comment)
+		}
+	nc_close(nc)
+}
