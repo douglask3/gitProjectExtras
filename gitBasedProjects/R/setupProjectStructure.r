928fb58fc685ff1bd3adaf5ff1aa8938720bfeaf
doug <douglas.kelley@students.mq.edu.au>
2015-12-07 21:51:25 +0000

    adding a function for initalising and setting up a new project

--- gitBasedProjects/R/setupProjectStructure.r
+++ gitBasedProjects/R/setupProjectStructure.r
@@ -2,3 +2,14 @@ setupProjectStructure <- function(names = paste(dirn,'_dir',sep=""),
 					                dirn = c('outputs', 'data', "libs",
 									         'temp', 'figs', 'docs'))
 	mapply(makeGlobDir, names, paste(dirn, '/', sep = ''))
+
+    
+setupProject <- function(dir = '.', initaliseGit = TRUE, remoteURL = NULL, setupStructure = TRUE, ...) {
+    if (dir != '.') {
+        makeDir(dir)
+        setwd()
+    }
+    if (initaliseGit) git.init()
+    if (remoteURL) git.remote(paset('add origin', remoteURL))
+    if (setupStructure) setupProjectStructure(...)
+}
