c2744394c492d98788d632cd19cf898573551f9c
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-03-01 18:49:04 +0000

    by default, files are no longer skipped if there is an error

--- gitBasedProjects/R/sourceAllLibs.r
+++ gitBasedProjects/R/sourceAllLibs.r
@@ -1,7 +1,11 @@
-sourceAllLibs <- function(path ='libs/', trace = TRUE, ...) {
+sourceAllLibs <- function(path ='libs/', trace = TRUE, stopIfError = FALSE,
+                          ...) {
 	for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
 		if(trace) cat(nm, ":")
-		try(source(file.path(path, nm), ...))
+
+		if (stopIfError) try(source(file.path(path, nm), ...))
+			else source(file.path(path, nm), ...)
+
 		if(trace) cat("\n")
 	}
 }
