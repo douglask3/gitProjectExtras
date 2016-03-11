82aa1f586eb84c722591899e006cb12df0b686ce
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-03-11 15:43:34 +0000

    adding in recursive option to find libs in sub dirs and setting default to TRUE

--- gitBasedProjects/R/sourceAllLibs.r
+++ gitBasedProjects/R/sourceAllLibs.r
@@ -1,6 +1,8 @@
-sourceAllLibs <- function(path ='libs/', trace = TRUE, stopIfError = TRUE,
-                          ...) {
-	for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
+sourceAllLibs <- function(path ='libs/', trace = TRUE, recursive = TRUE,
+                          stopIfError = TRUE, ...) {
+                              
+    files = list.files(path, pattern = "\\.[RrSsQq]$", recursive = recursive)
+	for (nm in files) {
 		if(trace) cat(nm, ":")
 
 		if (stopIfError) source(file.path(path, nm), ...)
